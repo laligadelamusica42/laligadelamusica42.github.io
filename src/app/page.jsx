3ef58b82175209e0a9907010a16ff89638e20067
Iván Dimitri Novomiast Dieguez <idimnovdie1602@protonmail.com>
2023-12-07 16:30:51 +0000

    ASD

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,17 @@
+"use client"
 import { Navbar } from '@/components/Navbar'
 import Image from 'next/image'
+import { useEffect } from 'react'
 
 export default function Home() {
+  const redirToVercel = () => {
+    if (window.location.href.includes('onrender.com')) {
+      window.location.href = 'https://laligadelamusica42.vercel.app/'
+    }
+  }
+  useEffect(() => {
+    redirToVercel();
+  });
   return (
     <>
       <header>
