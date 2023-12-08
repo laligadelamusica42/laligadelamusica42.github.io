05a004905a332df06e9ca76140115d5649a3ea72
Iván Dimitri Novomiast Dieguez <idimnovdie1602@protonmail.com>
2023-12-08 18:06:55 +0000

    OnDev Page Completed!

--- src/app/ondev/page.jsx
+++ src/app/ondev/page.jsx
@@ -1,5 +1,6 @@
 "use client"
 import React, { useEffect } from "react";
+import Image from 'next/image'
 
 const page = () => {
     const redirToVercel = () => {
@@ -12,7 +13,13 @@ const page = () => {
         redirToVercel();
     })
     return (
-        <div>OnDev</div>
+        <div className="container h-full">
+            <div className="flex items-center justify-center transform translate-y-[400px] font-bold text-neutral-200 text-xl">Proximamente:</div>
+            <div className="flex items-center justify-center h-screen">
+                <Image src={`/logo.png`} width={100} height={100} alt='laligadelamusica42_logo' className="ml-5" />
+                <div className="mr-5 text-4xl font-bold text-center text-white">La Liga de la Música</div>
+            </div>
+        </div>
     );
 }
 
