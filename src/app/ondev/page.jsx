7aa6fab4b0086d6c377aa2d30e1448ed78e86bb5
Iván Dimitri Novomiast Dieguez <idimnovdie1602@protonmail.com>
2023-12-08 18:10:23 +0000

    XD

--- src/app/ondev/page.jsx
+++ src/app/ondev/page.jsx
@@ -14,7 +14,7 @@ const page = () => {
     })
     return (
         <div className="container h-full">
-            <div className="flex items-center justify-center transform translate-y-[400px] font-bold text-neutral-200 text-xl">Proximamente:</div>
+            <div className="flex items-center justify-center transform translate-y-[300px] font-bold text-neutral-200 text-xl">Proximamente:</div>
             <div className="flex items-center justify-center h-screen">
                 <Image src={`/logo.png`} width={100} height={100} alt='laligadelamusica42_logo' className="ml-5" />
                 <div className="mr-5 text-4xl font-bold text-center text-white">La Liga de la Música</div>
