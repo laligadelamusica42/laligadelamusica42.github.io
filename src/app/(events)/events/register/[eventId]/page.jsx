4351e8d474197cd725bc2e25098a5ce53646515a
Iván Dimitri Novomiast Dieguez <idimnovdie1602@protonmail.com>
2023-12-10 12:31:10 +0000

    Refactor form submission and step navigation logic

--- src/app/(events)/events/register/[eventId]/page.jsx
+++ src/app/(events)/events/register/[eventId]/page.jsx
@@ -30,14 +30,18 @@ const changeStepStyle4 = (step, previousStep) => {
   return step === "4" ? activeStep : inactiveStep;
 };
 
-const handleSubmit = () => {
-  // split to get the dynamic route
-  const id = window.location.pathname.split("/")[3];
-  window.location.href = `/events/register/${id}?step=2`;
-}
-
 const changeStep = (step) => {
   if (step === "1") {
+    const handleSubmit = () => {
+      const sParams = useSearchParams();
+    
+      const step = sParams.get("step");
+      // split to get the dynamic route
+      const id = window.location.pathname.split("/")[3];
+      const newStep = (step === 1 || step === 2 || step === 3) ? step + 1 : 4;
+      console.log("newStep:", newStep);
+      window.location.href = `/events/register/${id}?step=${newStep}`;
+    }
     return (
       <main>
         <center>
@@ -96,13 +100,34 @@ const changeStep = (step) => {
               <br />
             </form>
             <div className="w-full">
-                <button className="mt-10 btn btn-primary w-80" onClick={() => handleSubmit()}>NEXT</button>
+                <button className="mt-10 btn btn-primary w-80" onClick={() => handleSubmit}>NEXT</button>
             </div>
           </div>
         </center>
       </main>
     )
   } else if (step === "2") {
+    const [selectedOption, setSelectedOption] = React.useState('0');
+
+    const handleSubmit = () => {
+      const sParams = useSearchParams();
+    
+      const step = sParams.get("step");
+      // split to get the dynamic route
+      const id = window.location.pathname.split("/")[3];
+      const newStep = (step === 1 || step === 2 || step === 3) ? step + 1 : 4;
+      console.log("newStep:", newStep);
+      window.location.href = `/events/register/${id}?step=${newStep}`;
+    }
+
+    const handleChange = (event) => {
+      setSelectedOption(event.target.value);
+    }
+
+    React.useEffect(() => {
+      console.log(selectedOption);
+    }, [selectedOption])
+
     return (
       <main>
         <center>
@@ -116,50 +141,24 @@ const changeStep = (step) => {
                   <div className="label">
                     <span className="label-text">Tus habilidades</span>
                   </div>
-                  <select className="w-full max-w-xs select select-bordered" value="">
-                    <option disabled selected>Selecciona una opción</option>
-                    <option value={1}>Sí</option>
-                    <option value={2}>No</option>
+                  <select className="w-full max-w-xs select select-bordered" defaultValue={0} onChange={handleChange} required={selectedOption === '0'}>
+                    <option disabled value={'0'}>Selecciona una opción</option>
+                    <option value={'1'}>Sí</option>
+                    <option value={'2'}>No</option>
                 </select>
                 </label>
                 <label className="mb-5 form-control w-80">
                   <div className="label">
                     <span className="label-text">¿Cuál?</span>
                   </div>
-                  <input
-                    type="text"
-                    placeholder="Type here"
-                    className="input input-bordered"
-                  />
+                  <textarea name="" id="" disabled={selectedOption === '2' || selectedOption === '0'} cols="30" rows="10" className={`textarea textarea-bordered ${selectedOption === '2' || selectedOption === '0' ? 'textarea-disabled' : ''}`} placeholder="Explicate aquí" required={selectedOption === '1'}></textarea>
                 </label>
               </div>
-              <label className="mb-5 w-80 form-control">
-                <div className="label">
-                  <span className="label-text">Usuario Intra</span>
-                </div>
-                <input
-                  type="text"
-                  placeholder="Type here"
-                  className="input input-bordered"
-                  required
-                />
-              </label>
-              <label className="mb-5 form-control w-80">
-                <div className="label">
-                  <span className="label-text">Email</span>
-                </div>
-                <input
-                  type="email"
-                  placeholder="Type here"
-                  className="input input-bordered"
-                  required
-                />
-              </label>
               <br />
               <br />
             </form>
             <div className="w-full">
-                <button className="mt-10 btn btn-primary w-80" onClick={() => handleSubmit()}>NEXT</button>
+              <button className="mt-10 btn btn-primary w-80" onClick={() => handleSubmit()}>NEXT</button>
             </div>
           </div>
         </center>
