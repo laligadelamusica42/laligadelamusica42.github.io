af7d4757dd8a26ea02d33037765bc5c229f4d8ad
Iván Dimitri Novomiast Dieguez <idimnovdie1602@protonmail.com>
2023-12-15 13:02:32 +0000

    Fix console.log issue in event registration page

--- src/app/(events)/events/register/[eventId]/page.jsx
+++ src/app/(events)/events/register/[eventId]/page.jsx
@@ -95,7 +95,7 @@ const handleRegister = async () => {
     "skills": skills?.skills,
     "eventId": eventId
   }
-  console.log("data:", formdata);
+  console.log("data:", JSON.parse(formdata));
   try {
     const response = await axios.post('/api/events/assistants', formdata);
     if (response.status === 200) {
